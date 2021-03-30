e7a41f382be8a23e2f36c2362b956af56700b885
moauris <mchenf@icloud.com>
2021-03-30 17:31:30 +0800

    Added User Model, implement basic security control for this app

---
+++ CtmaApp/Models/UserAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtmaApp.Models
+{
+    public class UserAccount
+    {
+        public long UserAccountID { get; set; }
+        public string UserName { get; set; }
+        public string Salt { get; set; }
+        public string SaltedHash { get; set; }
+    }
+}
